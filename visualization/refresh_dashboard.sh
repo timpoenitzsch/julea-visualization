1242a3bd0e6f4260752143f5f244ab4ee24a4aec
timpoenitzsch <tim.poenitzsch@gmail.com>
2024-10-25 14:59:29 +0200

    change comments

--- visualization/refresh_dashboard.sh
+++ visualization/refresh_dashboard.sh
@@ -5,13 +5,12 @@ BASE_DIR="../"
 
 echo "Starting HTTP server on port 8080 to serve CSV data..."
 cd "$BASE_DIR"
-python3 -m http.server 8080 &  # Start server im Hintergrund
+python3 -m http.server 8080 &  # Starte Server im Hintergrund
 
 # Warten, bis der HTTP-Server vollständig gestartet ist
-sleep 2  # Optional, falls der Server etwas Zeit zum Starten benötigt
+sleep 2  # Falls der Server etwas Zeit zum Starten benötigt
 
 echo "Sending data with curl"
 curl -X POST -H "Authorization: Bearer $GRAFANA_API_TOKEN" -H "Content-Type: application/json" -d @visualization/benchmark_dashboard.json http://localhost:3000/api/dashboards/db
 
-# Optional: Hintergrundprozess beenden
 kill $!  # Beendet den letzten im Hintergrund gestarteten Prozess (den HTTP-Server)
