b8927eb5e145d8b62307ed878a82196d3008f2da
timpoenitzsch <tim.poenitzsch@gmail.com>
2024-10-28 16:28:04 +0100

    script now uses argument

--- visualization/do_x_benchmarks.sh
+++ visualization/do_x_benchmarks.sh
@@ -9,8 +9,10 @@ VALUES_FILE="$ALERTING_DIR/values.csv"
 # Erstelle die Ordner für CSV-Dateien, falls sie nicht existieren
 mkdir -p "$CSV_DIR"
 
-# Führe 10 Benchmarks aus
-for i in {1..10}; do
+NUM_ITERATIONS=${1:-10}
+
+# Führe x Benchmarks aus
+for i in $(seq 1 $NUM_ITERATIONS); do
     # Erzeuge den Namen der neuen CSV-Datei mit dem aktuellen Unix-Timestamp
     UNIX_TIMESTAMP=$(date +%s)
     CSV_FILE="$CSV_DIR/benchmark_results_$UNIX_TIMESTAMP.csv"
