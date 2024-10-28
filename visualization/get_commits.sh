7939a664e20b74198436a9f574026e51fee5f5ba
timpoenitzsch <tim.poenitzsch@gmail.com>
2024-10-28 16:04:55 +0100

    adjust commit script

--- visualization/get_commits.sh
+++ visualization/get_commits.sh
@@ -1,2 +1,16 @@
-# Gib den Timestamp jedes Commits im Unix-Time-Format aus
-git log --pretty=format:'%H %at'
+#!/bin/bash
+
+# Name der CSV-Datei
+BASE_DIR="../"
+CSV_DIR="$BASE_DIR/commit_timestamps"
+OUTPUT_FILE="$CSV_DIR/commit_timestamps.csv"
+
+mkdir -p "$CSV_DIR"
+
+# Schreibe den Kopf in die CSV-Datei
+echo "SHA,timestamp" > "$OUTPUT_FILE"
+
+# Füge die Informationen der Commits hinzu
+git log --pretty=format:'%H,%at' >> "$OUTPUT_FILE"
+
+echo "Die Commit-Informationen wurden in $OUTPUT_FILE gespeichert."
