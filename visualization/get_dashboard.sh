8154764e63143c21ff5a7c0cbe7d2edba9b5cb99
timpoenitzsch <tim.poenitzsch@gmail.com>
2024-11-01 14:11:25 +0100

    add annotations to dashboard

--- visualization/get_dashboard.sh
+++ visualization/get_dashboard.sh
@@ -3,6 +3,8 @@
 # Setze den Basisordner
 BASE_DIR="../"
 
+"$BASE_DIR/visualization/get_commits.sh"
+
 echo "Starting HTTP server on port 8080 to serve CSV data..."
 cd "$BASE_DIR"
 python3 -m http.server 8080 &  # Starte Server im Hintergrund
