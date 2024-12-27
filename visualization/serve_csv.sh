d9793a7d52449e326d820f6a37b766789b6edde6
timpoenitzsch <tim.poenitzsch@gmail.com>
2024-12-27 13:42:36 +0100

    change comments

--- visualization/serve_csv.sh
+++ visualization/serve_csv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Setze den Basisordner
+# Set home directory
 BASE_DIR="../"
 
 "$BASE_DIR/visualization/get_commits.sh"
 
 echo "Starting HTTP server on port 8080 to serve CSV data..."
 cd "$BASE_DIR"
-python3 -m http.server 8080 # Starte Server
+python3 -m http.server 8080 
