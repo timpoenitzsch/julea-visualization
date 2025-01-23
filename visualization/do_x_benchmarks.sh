8556f90983849484ac7778a845267930a4467f13
timpoenitzsch <tim.poenitzsch@gmail.com>
2025-01-23 18:49:22 +0100

    keep errors out of csv, create logs

--- visualization/do_x_benchmarks.sh
+++ visualization/do_x_benchmarks.sh
@@ -2,11 +2,11 @@
 
 BASE_DIR="../"
 CSV_DIR="$BASE_DIR/benchmark_csv"
-ALERTING_DIR="$BASE_DIR/alerting"
-VALUES_FILE="$ALERTING_DIR/values.csv"
+LOG_DIR="$BASE_DIR/benchmark_logs"
 
-# Create folder for csv-files
-mkdir -p "$CSV_DIR"
+
+# Create directories
+mkdir -p "$CSV_DIR" "$LOG_DIR"
 
 NUM_ITERATIONS=${1:-1}
 SLEEP_TIME=${2:-600}
@@ -16,10 +16,21 @@ for i in $(seq 1 $NUM_ITERATIONS); do
 
     UNIX_TIMESTAMP=$(date +%s)
     CSV_FILE="$CSV_DIR/benchmark_results_$UNIX_TIMESTAMP.csv"
+    LOG_FILE="$LOG_DIR/benchmark_log_$UNIX_TIMESTAMP.log"
     
-    ../scripts/benchmark.sh -m > "$CSV_FILE"
+    #execute benchmark and save information in log- and CSV-file
+    ../scripts/benchmark.sh -m 2> "$LOG_FILE" | \
+        # if dependencies were not loaded, respective messages are not saved in CSV-File
+        grep -v '^Dependency "' \
+        > "$CSV_FILE"
     
+    # If log is empty (no errors occured), create message
+    if [ ! -s "$LOG_FILE" ]; then
+        echo "Es sind keine Fehlermeldungen aufgetreten." > "$LOG_FILE"
+    fi
+
     echo "CSV-Datei erstellt: $CSV_FILE"
+    echo "log-Datei erstellt: $LOG_FILE"
     echo "Timestamp (Unix-Zeit): $UNIX_TIMESTAMP"
     
     # wait until next benchmark
