ce9838cbf7eb7f6f6e4963712a8fba3bf9f8d0e0
timpoenitzsch <56826463+timpoenitzsch@users.noreply.github.com>
2025-01-30 15:07:03 +0100

    englische Kommentare

--- visualization/get_dashboard.sh
+++ visualization/get_dashboard.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Setze den Basisordner
+# Set base directory
 BASE_DIR="../"
 
 "$BASE_DIR/visualization/get_commits.sh"
 
 echo "Starting HTTP server on port 8080 to serve CSV data..."
 cd "$BASE_DIR"
-python3 -m http.server 8080 &  # Starte Server im Hintergrund
+python3 -m http.server 8080 &  # start server in background
 
-# Warten, bis der HTTP-Server vollständig gestartet ist
-sleep 2  # Falls der Server etwas Zeit zum Starten benötigt
+# wait until server is fully started
+sleep 2  
 
 echo "Sending data with curl"
 curl -X POST -H "Authorization: Bearer $GRAFANA_API_TOKEN" -H "Content-Type: application/json" -d @visualization/benchmark_dashboard.json http://localhost:3000/api/dashboards/db
