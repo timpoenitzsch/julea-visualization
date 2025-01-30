d60a9736f64cdc064f910e95aefa7d6a6dfca20e
timpoenitzsch <56826463+timpoenitzsch@users.noreply.github.com>
2025-01-30 14:24:56 +0100

    remove getting commits from script.

--- visualization/serve_csv.sh
+++ visualization/serve_csv.sh
@@ -3,8 +3,6 @@
 # Set home directory
 BASE_DIR="../"
 
-"$BASE_DIR/visualization/get_commits.sh"
-
 echo "Starting HTTP server on port 8080 to serve CSV data..."
 cd "$BASE_DIR"
 python3 -m http.server 8080 
